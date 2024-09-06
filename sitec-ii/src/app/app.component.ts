79ccaf5801280f0b135d6053292b055d9fe5981a
nordivi <victorw.nordi@icloud.com>
2024-09-05 21:26:48 -0300

    :sparkles: main-content

--- sitec-ii/src/app/app.component.ts
+++ sitec-ii/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { MainContentComponent } from "./main-content/main-content.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, NavBarComponent],
+  imports: [RouterOutlet, HeaderComponent, NavBarComponent, MainContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
