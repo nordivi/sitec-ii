57c5002ab484b8d6d7e634d9a7f4fe8a13956be7
nordivi <victorw.nordi@icloud.com>
2024-09-10 20:47:46 -0300

    :sparkles: conteudo explicativo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { NavBarComponent } from "./nav-bar/nav-bar.component";
 import { MainContentComponent } from "./main-content/main-content.component";
+import { ExplanatoryContentComponent } from "./explanatory-content/explanatory-content.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, NavBarComponent, MainContentComponent],
+  imports: [RouterOutlet, HeaderComponent, NavBarComponent, MainContentComponent, ExplanatoryContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
