4f9b50dc759d6fa3f56be157f4e7e7c8164fc944
nordivi <victorw.nordi@icloud.com>
2024-09-13 21:45:04 -0300

    ♻️ refactor code

--- src/app/scheduler/scheduler.component.ts
+++ src/app/scheduler/scheduler.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { CalendarComponent } from "../calendar/calendar.component";
 
 @Component({
   selector: 'app-scheduler',
   standalone: true,
-  imports: [],
+  imports: [CalendarComponent],
   templateUrl: './scheduler.component.html',
   styleUrl: './scheduler.component.css'
 })
 export class SchedulerComponent {
 
+showCheckboxes() {
+  var expanded = false;
+  var checkboxes = document.getElementById("checkboxes");
+ if(checkboxes)
+  { if (!expanded) {
+    checkboxes.style.display = "block";
+    expanded = true;
+  } else {
+    checkboxes.style.display = "none";
+    expanded = false;
+  }}
+}
 }
